e7c784feed2789513bd8880c2fa2ea9049d64f58
Ryan <leeyuenhigh@gmail.com>
2021-03-07 15:48:13 -0500

    Renamed files

--- search/src/index.js
+++ search/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LinearSearch from './App';
+import LinearSearch from './linear-search';
 import './index.css';
 
 ReactDOM.render(<LinearSearch />, document.getElementById('root'));
